3548388556ccd80debe5bbb4bf231b2919f0cb4d
z2014 <591763389@qq.com>
2018-05-26 17:30:36 +0800

    class component

--- simple-react/index.js
+++ simple-react/index.js
@@ -1,2 +1,28 @@
+/** @jsx Gooact.createElement */
 import Gooact, {render, Component} from 'gooact';
-render(<div>Hello World!</div>, document.body);
+// const App = (props) => {
+// 	return (<div>{props.name}</div>)
+// }
+class App extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			num: 3
+		}
+		this.click = this.click.bind(this)
+	}
+	click() {
+		this.setState({
+			num: 9
+		})
+	}
+	render() {
+		return (
+			<div>
+				<div onClick={this.click}>click</div>
+				{this.state.num}
+			</div>
+			)
+	}
+}
+render(<App name="zcl"/>, document.body);
